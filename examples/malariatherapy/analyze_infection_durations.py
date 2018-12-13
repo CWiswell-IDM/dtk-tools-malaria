bbf39071790dc0247bde3b0b591690a612c14c3a
CWiswell-IDM <cwiswell@intven.com>
2018-12-13 14:51:08 -0800

    updating as per JGerardin feedback

--- examples/malariatherapy/analyze_infection_durations.py
+++ examples/malariatherapy/analyze_infection_durations.py
@@ -17,36 +17,41 @@ Institute for Disease Modeling, Bellevue, WA
 from simtools.Analysis.BaseAnalyzers import BaseAnalyzer
 import pandas as pd
 import numpy as np
-from os import path
+import os
 import matplotlib.pyplot as plt
 
 class DurationsAnalyzer(BaseAnalyzer):
     def __init__(self, weight=1):
         super().__init__(filenames = ['output/MalariaPatientReport.json'])
         self.channel = 'true_asexual_parasites'
-        self.datagroupnames = ['sample', 'sim_id', 'channel']
+        # self.datagroupnames = ['sample', 'sim_id', 'channel']
 
+    # df People = Columns, Days = rows, cells: true parasites
     def select_simulation_data(self, data, simulation):
         patient_data = data[self.filenames[0]]['patient_array']
         patient_df = pd.DataFrame({'patient %d' % x['id']: x[self.channel][0] for x in patient_data})
-        patient_df.reset_index(drop=True)
+        # patient_df.reset_index(drop=True)
         return patient_df
 
     def finalize(self, all_data):
         bins = np.linspace(0,365,12)
+        # Looping through sims
         for sim, data in all_data.items():
-            fig, ax = plt.subplots(1, 1)
+            sim_id = sim.id[0:3]
             sf = sim.tags.get("scale_factor")
             seed = sim.tags.get("Run_Number")
-            sim_id = sim.id[0:3]
+            figure_name = f"{sim_id}_{sf}_{seed}"
+            fig= plt.figure(figure_name)
+            ax = fig.gca()
             simulation_durations = []
 
+            # Looping through people
             for column in data.columns:
                 (positive_day_indices,) = np.where(np.array(data[column]) >0)
                 try:
-                    simulation_durations.append(max(positive_day_indices))
+                    simulation_durations.append(max(positive_day_indices)) # Last day with parasite
                 except:
-                    print(sf,seed)
+                    print(f"EXCEPTION: ScaleFactor : {sf}, Seed: {seed}")
 
             ax.hist(simulation_durations,bins = bins, normed=True,alpha= 0.5)
             ax.set_ylim([0,0.01])
@@ -56,7 +61,7 @@ class DurationsAnalyzer(BaseAnalyzer):
             ax.set_xlabel('Infection Duration (days)')
             ax.set_ylabel('Frequency')
 
-            plt.savefig(path.join(self.working_dir, f"{sim_id}_{sf}_{seed}.eps"))
-            plt.show()
+            plt.savefig(os.path.join(self.working_dir, f"{figure_name}.png"))
+        plt.show() #Make sure I see 4
 
 
