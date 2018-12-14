28abe093b9dec0a78e8bf2f6c2d12bfe2e004690
Jaline Gerardin <jgerardin@idmod.org>
2018-12-14 13:47:33 -0800

    mild cleanup on sahel run script

--- examples/simple_1node_drug_interventions/run_with_interventions.py
+++ examples/simple_1node_drug_interventions/run_with_interventions.py
@@ -1,13 +1,18 @@
 from malaria.study_sites.site_setup_functions import summary_report_fn
 from dtk.utils.core.DTKConfigBuilder import DTKConfigBuilder
 from malaria.interventions.malaria_drug_campaigns import add_drug_campaign
+from malaria.reports.MalariaReport import add_summary_report
+
 from simtools.ExperimentManager.ExperimentManagerFactory import ExperimentManagerFactory
 from simtools.ModBuilder import ModBuilder, ModFn
 from simtools.SetupParser import SetupParser
-from examples.simple_1node_drug_interventions.configure_sahel_intervention_system import configure_sahel_intervention_system
+from configure_sahel_intervention_system import configure_sahel_intervention_system
+
+from simtools.Analysis.AnalyzeManager import AnalyzeManager
+from prevalence_by_age_analyzer import PrevalenceAnalyzer
+
 
 import os
-
 
 
 
@@ -29,7 +34,7 @@ def add_smc_group(cb, coverage=1.0, start_days=[60, 60+365], agemax=10, drug='DP
     return {'Coverage': coverage, 'Start': start_days[0], 'Intervention_type': 'SMC%i' %agemax}
 
 
-def add_summary_report(cb, start_day=0):
+def add_summary_report_by_start_day(cb, start_day=0):
     '''
     Add daily summary report to output
     :param cb: config builder
@@ -37,14 +42,14 @@ def add_summary_report(cb, start_day=0):
     :return: None
     '''
 
-    summary_report_fn(start=start_day+1, interval=1.0, description='Daily_Report',
-                      age_bins=[5.0, 10.0, 100.0])(cb)
+    add_summary_report(cb, start=start_day+1, interval=1.0, description='Daily_Report',
+                       age_bins=[5.0, 10.0, 100.0])
 
     return {'report_start_day': start_day}
 
 
 sim_duration = 1    # in years
-num_seeds = 1
+num_seeds = 10
 
 # Run create_serialized_file.py to generate serialized file
 
@@ -62,23 +67,20 @@ cb.update_params({'Serialized_Population_Filenames': ['serialized_sahel.dtk'],
                   })
 
 # Set up interventions
-intervention_days = [180]
+smc_start_day = 180
 coverages = [0.6, 0.7, 0.8, 0.9, 1.0]
 
 SMC = [
             [
-               # ModFn(DTKConfigBuilder.set_param, 'Run_Number', seed),
-               # ModFn(DTKConfigBuilder.set_param, 'Simulation_Duration', smc_start_day+365),
-               # ModFn(add_smc_group,
-               #           start_days=[smc_start_day],
-               #           coverage=smc_coverage, drug=drug, agemax=agemax),
-               ModFn(add_summary_report, start_day=smc_start_day),
+               ModFn(DTKConfigBuilder.set_param, 'Run_Number', seed),
+               ModFn(DTKConfigBuilder.set_param, 'Simulation_Duration', smc_start_day+365),
+               ModFn(add_smc_group,
+                         start_days=[smc_start_day],
+                         coverage=smc_coverage, drug='DP', agemax=5),
+               ModFn(add_summary_report_by_start_day, start_day=smc_start_day),
             ]
-           for smc_start_day in intervention_days
-           # for smc_coverage in coverages
-           # for seed in range(num_seeds)
-           # for agemax in [5]
-           # for drug in ['DP']
+           for smc_coverage in coverages
+           for seed in range(num_seeds)
         ]
 
 builder = ModBuilder.from_list(SMC)
@@ -98,3 +100,11 @@ if __name__ == "__main__":
     # Wait for the simulations to be done
     exp_manager.wait_for_finished(verbose=True)
     assert (exp_manager.succeeded())
+
+    analyzer = PrevalenceAnalyzer(expt_name=expname,
+                                  sweep_variables=["Run_Number",
+                                                   "Coverage"
+                                                   ])
+
+    am = AnalyzeManager(exp_manager.experiment, analyzers=analyzer)
+    am.analyze()
