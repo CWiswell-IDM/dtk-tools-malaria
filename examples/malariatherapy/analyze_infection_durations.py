72da6abb520bad7bd31a13d89506eb3cf9ef5655
CWiswell-IDM <cwiswell@intven.com>
2018-12-14 10:13:46 -0800

    Refactoring to meet malaria team's sample conventions. pylint looks better, but I could use help on a docstring for run_malariatherapy_dtk.set_immune_forcing_builder() and what should be in the README.txt

--- examples/malariatherapy/analyze_infection_durations.py
+++ examples/malariatherapy/analyze_infection_durations.py
@@ -2,8 +2,9 @@
 """
 title: analyze_infection_durations.py
 
-description: An analyzer aimed to plotthe distribution of infection durations (total days with patent parasitemia from
-a challenge bite) that are emitted via the Malaria Patient Report output from DTK simulations.
+description: An analyzer aimed to plotthe distribution of infection durations (total days with
+patent parasitemia from a challenge bite) that are emitted via the Malaria Patient Report output
+from DTK simulations.
 
 author: Jon Russell
 
@@ -21,47 +22,51 @@ import os
 import matplotlib.pyplot as plt
 
 class DurationsAnalyzer(BaseAnalyzer):
-    def __init__(self, weight=1):
-        super().__init__(filenames = ['output/MalariaPatientReport.json'])
+    """
+    Generates histogram of infectious durations by scale factor
+
+    Attributes:
+        channel: channel to load from patient data (true_asexual_parasites)
+        show_plots: True displays and saves graphs to disk, False only saves them
+    """
+    def __init__(self, show=True):
+        super().__init__(filenames=['output/MalariaPatientReport.json'])
         self.channel = 'true_asexual_parasites'
-        # self.datagroupnames = ['sample', 'sim_id', 'channel']
+        self.show_plots = show
 
     # df People = Columns, Days = rows, cells: true parasites
     def select_simulation_data(self, data, simulation):
         patient_data = data[self.filenames[0]]['patient_array']
         patient_df = pd.DataFrame({'patient %d' % x['id']: x[self.channel][0] for x in patient_data})
-        # patient_df.reset_index(drop=True)
         return patient_df
 
     def finalize(self, all_data):
-        bins = np.linspace(0,365,12)
+        bins = np.linspace(0, 365, 12)
         # Looping through sims
         for sim, data in all_data.items():
             sim_id = sim.id[0:3]
             sf = sim.tags.get("scale_factor")
-            seed = sim.tags.get("Run_Number")
-            figure_name = f"{sim_id}_{sf}_{seed}"
-            fig= plt.figure(figure_name)
+            figure_name = f"scale-{sf}_sim-{sim_id}"
+            fig = plt.figure(figure_name)
             ax = fig.gca()
             simulation_durations = []
 
             # Looping through people
             for column in data.columns:
-                (positive_day_indices,) = np.where(np.array(data[column]) >0)
+                (positive_day_indices,) = np.where(np.array(data[column]) > 0)
                 try:
                     simulation_durations.append(max(positive_day_indices)) # Last day with parasite
                 except:
-                    print(f"EXCEPTION: ScaleFactor : {sf}, Seed: {seed}")
+                    print(f"EXCEPTION: ScaleFactor: {sf}, column(person): {column}")
 
-            ax.hist(simulation_durations,bins = bins, normed=True,alpha= 0.5)
-            ax.set_ylim([0,0.01])
-            ax.annotate("Mean = %s" %np.mean(simulation_durations),xy = (250,0.009),size= 'large')
+            ax.hist(simulation_durations, bins=bins, normed=True, alpha=0.5)
+            ax.set_ylim([0, 0.01])
+            ax.annotate("Mean = %s" %np.mean(simulation_durations), xy=(250, 0.009), size='large')
             ax.annotate(f"ScaleFactor = {sf}", xy=(250, 0.008), size='large')
-            
+
             ax.set_xlabel('Infection Duration (days)')
             ax.set_ylabel('Frequency')
 
             plt.savefig(os.path.join(self.working_dir, f"{figure_name}.png"))
-        plt.show() #Make sure I see 4
-
-
+        if self.show_plots:
+            plt.show()
--- examples/malariatherapy/immunity_transitions_configuration.py
+++ examples/malariatherapy/immunity_transitions_configuration.py
@@ -1,27 +1,37 @@
-import numpy as np
 import copy
+import numpy as np
 
 
-def probability_shifting_module(TM, immune_stimulation_threshold,row_scale_factor,column_scale_factor):
+def probability_shifting_module(TM,
+                                immune_stimulation_threshold,
+                                row_scale_factor,
+                                column_scale_factor):
     number_of_bin_edges = max([len(i) for i in TM])
     bin_numbers = np.arange(0, number_of_bin_edges)
     new_TM = copy.copy(TM)
     rows_to_cycle_through = np.arange(immune_stimulation_threshold, max(bin_numbers) + 1)
     # define a row scaling factor that represents the changing force of immunity across current density classes
     for row in rows_to_cycle_through:
-        Old_column_values = [x for x in TM[row]]
-        Bin_decreases = [np.log10(column_scale_factor * row_scale_factor * bin) for bin in bin_numbers]
-        New_column_values = [Old_column_values[i] / Bin_decreases[i] for i in range(len(Old_column_values))]
+        old_column_values = [x for x in TM[row]]
+        bin_decreases = [np.log10(column_scale_factor * row_scale_factor * bin)
+                         for bin in bin_numbers]
+        new_column_values = [old_column_values[i] / bin_decreases[i]
+                             for i in range(len(old_column_values))]
 
-        Net_zero_column_change = sum(
-            [abs(New_column_values[j] - Old_column_values[j]) for j in np.arange(1, len(Old_column_values))])
-        New_column_values[0] = Old_column_values[0] + Net_zero_column_change
-        new_TM[row] = New_column_values
+        net_zero_column_change = sum(
+            [abs(new_column_values[j] - old_column_values[j])
+             for j in np.arange(1, len(old_column_values))])
+        new_column_values[0] = old_column_values[0] + net_zero_column_change
+        new_TM[row] = new_column_values
 
-    return(TM,new_TM)
+    return(new_TM)
 
-def set_transition_matrix(cb,TM,scale_factor,immune_stim_threshold = 1):
-    old_TM, shifted_TM = probability_shifting_module(TM,immune_stim_threshold, row_scale_factor= scale_factor,column_scale_factor= scale_factor)
+
+def set_transition_matrix(cb, TM, scale_factor, immune_stim_threshold=1):
+    shifted_TM = probability_shifting_module(TM,
+                                             immune_stim_threshold,
+                                             row_scale_factor=scale_factor,
+                                             column_scale_factor=scale_factor)
     cb.update_params({"Parasite_Peak_Density_Probabilities": shifted_TM,
                       ".Scale_Factor": scale_factor})
 
